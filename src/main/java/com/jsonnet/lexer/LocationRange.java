3f85f188c0d2961c53c949b7ac85b89322defa69
mqliang <secret.mqliang@gmail.com>
2023-01-06 23:34:41 -0800

    Implement the lexer

---
+++ src/main/java/com/jsonnet/lexer/LocationRange.java
@@ -0,0 +1,37 @@
+package com.jsonnet.lexer;
+
+// LocationRange represents a range of a source file.
+class LocationRange {
+  String fileName;
+  Location begin;
+  Location end;
+
+  public LocationRange(String fileName, Location begin, Location end) {
+    this.fileName = fileName;
+    this.begin = begin;
+    this.end = end;
+  }
+
+  // IsSet returns if this LocationRange has been set.
+  boolean isSet() {
+    return this.begin.isSet();
+  }
+
+  @Override
+  public String toString() {
+    if (!isSet()) {
+      return this.fileName;
+    }
+    String filePrefix = "";
+    if (this.fileName != null && this.fileName.length() > 0) {
+      filePrefix = this.fileName + ":";
+    }
+    if (this.begin.line == this.end.line) {
+      if (this.begin.column == this.end.column) {
+        return String.format("%s%s", filePrefix, this.begin);
+      }
+      return String.format("%s%s-%s", filePrefix, this.begin, this.end.column);
+    }
+    return String.format("%s(%s)-(%s)", filePrefix, this.begin, this.end);
+  }
+}
---
+++ src/main/java/com/jsonnet/lexer/fodder/FodderKind.java
@@ -0,0 +1,9 @@
+package com.jsonnet.lexer.fodder;
+
+// FodderKind defines kinds of fodder element
+public enum FodderKind {
+  fodderWhitespace,
+  fodderCommentC,
+  fodderCommentCpp,
+  fodderCommentHash
+}
